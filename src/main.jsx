c08ff0c4209016b6a4167530d5d9b0218420d88f
Nicholas Williams <nicholaswilliams910@gmail.com>
2023-11-13 13:40:16 -0600

    added conditional delete operation

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ApplicationViews } from './components/ApplicationViews'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApplicationViews } from "./components/ApplicationViews";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApplicationViews />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
